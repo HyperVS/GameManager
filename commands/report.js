8f71bd3e6093b1bd134f704e3fcbfe1601b638ab
HyperVS <63892536+HyperVS@users.noreply.github.com>
2021-01-05 04:10:05 +0300

    Add mmr calculation, begin to fix queue flow

--- commands/report.js
+++ commands/report.js
@@ -9,7 +9,9 @@ module.exports = {
 	args: true,
 	usage: `${prefix}report [W/L]`,
 	execute(client, message, args){
-		if(!global.findInMap(client.matches, message.author.id)) return;
+		if(!global.findInMap(client.matches, message.author.id)) return; 
+		//TODO: this needs to search the specifc game the user is in
+		//TODO: currently it checks if a user is in a game
 		//let matchID = parseInt(global.parseMatchId(message.channel.name));
 		let count = client.matches.get(message.channel.name);
 		let players = Array.from(count.entries());
