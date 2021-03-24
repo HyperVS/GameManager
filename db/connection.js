0765830a1f03bab82e2f482694cf3a4f1d0b3d82
Mercenary <mercenary.net@outlook.com>
2021-03-24 01:50:00 -0500

    orm fixes

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+const host = process.env.HOST || "96.58.110.215";
 
 var connection = mysql.createConnection({
-    host: '192.168.1.78',
-    user: 'access',
-    password: 'zKuh7Wfb'
+    host: host,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = connection;
